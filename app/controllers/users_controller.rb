dfcdebc59c5db1301de880f7231c6ff4a55839ea
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-23 19:36:47 -0500

    created views for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :delete]
+
+  def new 
+    @user = User.new
+  end
+
+  def create 
+    @user = User.create(user_params)
+  end
+
+  def show
+    
+  end 
+
+  def edit 
+
+  end 
+
+  def update 
+    @user.update(user_params)
+  end
+
+  def delete 
+    @user.destroy
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
