a8a81fde916ff7c31f56f4b5b77875969ccdd9cf
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-23 19:55:18 -0500

    create views for conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,37 @@
 class ConversationsController < ApplicationController
+  before_action :set_convo, only: [:show, :edit, :update, :delete]
+
+  def new 
+    @convo = Conversation.new
+  end
+
+  def create 
+    @convo = Conversation.create(convo_params)
+  end
+
+  def show 
+
+  end
+  
+  def edit
+
+  end
+
+  def update
+    @convo.update(convo_params)
+  end
+
+  def delete 
+    @convo.destroy
+  end
+
+  private 
+
+  def set_convo 
+    @convo = Conversation.find(params[:id])
+  end
+
+  def convo_params
+    params.require(:user).permit
+  end
 end
