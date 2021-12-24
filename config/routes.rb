1d6504be0d256bf579972c1cc11b9836017f8e95
RTBlanco <ronnytoribio1@hotmail.com>
2021-12-23 19:38:47 -0500

    nested the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :messages
-  resources :conversations
-  resources :users
+  
+  resources :users do 
+    resources :conversations do
+      resources :messages 
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
